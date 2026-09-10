e25a16c6116f50b7fd0683525089607fe535c41c
inka-moss <angel.pomatay.p@gmail.com>
2026-09-10 09:32:31 -0500

    Actualizar nombre del sistema a TRANSNEL

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" type="image/jpeg" href="{{ asset('logo.jpeg') }}">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'TRANSNEL') }}</title>
 
         @routes
         @viteReactRefresh
